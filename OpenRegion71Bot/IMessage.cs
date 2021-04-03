0e89c2f42b6bc54ee996a7fcdfa4f7a5055fa11a
tatar-in <khrr22@yandex.ru>
2021-04-03 22:42:40 +0300

    first commit

---
+++ OpenRegion71Bot/IMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenRegion71Bot
+{
+    interface IMessage
+    {
+        /// <summary>
+        /// Information about the message 
+        /// </summary>
+        /// <param name="id">ID of the message</param>
+        void InformationAboutMessage(int id);
+    }
+}
---
+++ OpenRegion71Bot/IReport.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenRegion71Bot
+{
+    interface IReport
+    {
+        /// <summary>
+        /// Weekly report for Komrakova
+        /// </summary>
+        void WeeklyReport();
+    }
+}
