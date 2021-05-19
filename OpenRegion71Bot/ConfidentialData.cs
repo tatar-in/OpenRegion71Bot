3f8e0b81b5677c4fd861b11857c73ffa0687fa83
tatar-in <khrr22@yandex.ru>
2021-05-19 19:27:38 +0300

    подключен telegram бот и добавлен файл с конфиденциальной информацией

---
+++ OpenRegion71Bot/ConfidentialData.cs
@@ -0,0 +1,28 @@
+﻿namespace OpenRegion71Bot
+{
+    class ConfidentialData
+    {
+        public static string BotTelegramToken { get => "скрытая строка"; }
+        public static string BotDataBase { get => "скрытая строка"; }
+        public static string BotLogs { get => "скрытая строка"; }
+        public class OpenRegion71
+        {
+            public static string AuthPage { get => "скрытая строка"; }
+            public static string Login { get => "скрытая строка"; }
+            public static string Password { get => "скрытая строка"; }
+            public static string ExecutorsStructurePages { get => "скрытая строка"; }
+            public static string ExecutorStructureDetailPage { get => "скрытая строка"; }
+            public static string ProblemPage { get => "скрытая строка"; }
+            public static string SourceCategoryPages { get => "скрытая строка"; }
+            public static string SourcePages { get => "скрытая строка"; }
+            public class Api
+            {
+                private static string Key { get => "скрытая строка"; }
+                public static string FRayons { get => "скрытая строка" + Key; }
+                public static string Themes { get => "скрытая строка" + Key; }
+                public static string Problems { get => "скрытая строка" + Key; }
+            }
+        }
+
+    }
+}
--- OpenRegion71Bot/Program.cs
+++ OpenRegion71Bot/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Args;
 
 namespace OpenRegion71Bot
 {
     class Program
     {
+        private static readonly TelegramBotClient bot = new TelegramBotClient(ConfidentialData.BotTelegramToken);
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bot.OnMessage += Bot_OnMessage;
+            bot.OnMessageEdited += Bot_OnMessageEdited;
+            bot.StartReceiving();
+            Console.ReadLine();
+            bot.StopReceiving();
+        }
+        private static async void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Text)
+            {
+                if (e.Message.Text == "как дела")
+                {
+                    await bot.SendTextMessageAsync(e.Message.Chat.Id, "спасибо, хорошо", replyToMessageId: e.Message.MessageId);
+                }
+                else
+                {
+                    await bot.SendTextMessageAsync(e.Message.Chat.Id, "Неизвестная команда", replyToMessageId: e.Message.MessageId);
+                }
+            }
+            else
+            {
+                await bot.SendTextMessageAsync(e.Message.Chat.Id, "Данный тип сообщений не поддерживается, разрешены только текстовые сообщения.", replyToMessageId: e.Message.MessageId);
+            }
+        }
+        private static async void Bot_OnMessageEdited(object sender, MessageEventArgs e)
+        {
+            await bot.SendTextMessageAsync(e.Message.Chat.Id, "Изменение сообщений не поддерживается.", replyToMessageId: e.Message.MessageId);
         }
     }
 }
